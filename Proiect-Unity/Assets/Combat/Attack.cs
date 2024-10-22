62a4eb69755d1b8931b6501ac82bf69ab0ff13ef
Hutanu Eusebiu-Ciprian <eusebiuciprian.hutanu03@gmail.com>
2024-10-22 23:21:08 +0300

    First combat system version
    Not finished yet! I need to use the input system for initiating an attack. Also I need to connect the player to the PlayerStats script. For the future: restructure the logic behind the AttackAction, for multi-champion implementation.

---
+++ Proiect-Unity/Assets/Combat/Attack.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    //Standard class for all the attacks we will think about
+
+    public float damage;
+    public float range;
+    public float cooldown;
+    public float baseKnockback;
+
+    private bool isOnCooldown;
+
+    public LayerMask enemy;
+
+    public Attack(float _damage, float _range, float _cooldown, float _baseKnockback)
+    {
+        this.damage = _damage;
+        this.range = _range;
+        this.cooldown = _cooldown;
+        this.baseKnockback = _baseKnockback;
+
+        enemy = LayerMask.GetMask("Player");
+        isOnCooldown = false;
+    }
+
+    public void PerformAttack(Vector2 playerPosition, Vector2 direction)
+    {
+        if (!isOnCooldown)
+        {
+            Vector2 offsetPostion = playerPosition + direction;
+
+            RaycastHit2D hit = Physics2D.Raycast(offsetPostion, direction, range);
+
+            //Debug.Log(playerPosition + " " +  direction + " " + range);
+            
+            if (hit.collider != null)
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+                if (hit.collider.GetComponent<PlayerStats>() != null)
+                {
+                    hit.collider.GetComponent<PlayerStats>().TakeDamage(damage, baseKnockback, direction);
+                }
+            }
+
+            //isOnCooldown=true;
+
+            //Invoke("AttackCooldown", cooldown);
+        }
+    }
+
+    void AttackCooldown()
+    {
+        isOnCooldown = false;
+    }
+}
