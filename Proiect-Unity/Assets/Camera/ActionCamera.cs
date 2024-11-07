dd101d7f29e084b9f498478e0b4825645c35d40a
Codarcea Alexandru-Christian <alexandru.codarcea@gmail.com>
2024-11-07 11:38:25 +0200

    camera v1.1
    second prototype of camera. it is working

---
+++ Proiect-Unity/Assets/Camera/ActionCamera.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Camera))]
+public class ActionCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float maxZoom = 40f;
+    public float minZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    private Vector3 velocity;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(minZoom, maxZoom, GetGreatestDistance() / zoomLimiter);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
+
+        float newZoom2 = Mathf.Lerp(minZoom, maxZoom, GetGreatestDistance2() / zoomLimiter);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom2, Time.deltaTime);
+
+    }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+
+    }
+
+    float GetGreatestDistance2()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.y;
+
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+
+    // Methods to add and remove players
+    public void AddPlayer(PlayerInput playerInput)
+    {
+        if (!targets.Contains(playerInput.transform))
+        {
+            targets.Add(playerInput.transform);
+        }
+    }
+
+    public void RemovePlayer(PlayerInput playerInput)
+    {
+        targets.Remove(playerInput.transform);
+    }
+}
