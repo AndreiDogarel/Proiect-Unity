fca43cfff64c5d7118cfd841bfdba291d7fbed32
Codarcea Alexandru-Christian <alexandru.codarcea@gmail.com>
2024-11-12 17:43:30 +0200

    functional minimal start menu
    v1.0

---
+++ Proiect-Unity/Assets/StartMenu/StartMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Map1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
