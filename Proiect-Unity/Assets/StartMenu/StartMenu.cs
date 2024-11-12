a8f1346f8545d81916466753c1dc37fe8e255a08
Codarcea Alexandru-Christian <alexandru.codarcea@gmail.com>
2024-11-13 00:15:34 +0200

    start menu done and 100% functional
    implemented a start menu, fully functional + UI elements + selection of game enviroment (map + no. of players)

--- Proiect-Unity/Assets/StartMenu/StartMenu.cs
+++ Proiect-Unity/Assets/StartMenu/StartMenu.cs
@@ -1,17 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
-   public void PlayGame()
+    private CounterManager counterManager;
+
+    public Image V1; 
+    public Image V2;
+    public Image V3;
+
+    private void Start()
     {
-        SceneManager.LoadSceneAsync("Map1");
+        counterManager = FindObjectOfType<CounterManager>();
+    }
+
+    public void PlayGame()
+    {
+        int playerCount = counterManager.GetCurrentValue();
+        SetPlayerCount(playerCount);
+
+        int levelIndex = GetActiveMapIndex();
+        SceneManager.LoadSceneAsync("Map" + levelIndex);
+    }
+
+    public void SetPlayerCount(int playerCount)
+    {
+        PlayerPrefs.SetInt("PlayerCount", playerCount);
+        PlayerPrefs.Save();
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private int GetActiveMapIndex()
+    {
+        if (V1.gameObject.activeSelf)
+        {
+            return 1;
+        }
+        else if (V2.gameObject.activeSelf)
+        {
+            return 2;
+        }
+        else if (V3.gameObject.activeSelf)
+        {
+            return 3;
+        }
+
+        return 1;
     }
 }
