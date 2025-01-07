fc8e2a03c5ff770f799f1c6f9a3cf16450c65f22
Codarcea Alexandru-Christian <alexandru.codarcea@gmail.com>
2025-01-07 14:29:26 +0200

    resolution setting working (not visible in editor)

--- Proiect-Unity/Assets/OptionsMenu/SettingsMenu.cs
+++ Proiect-Unity/Assets/OptionsMenu/SettingsMenu.cs
@@ -2,10 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        foreach (Resolution resolution in resolutions) {
+            string option = resolution.width + " x " + resolution.height;
+            options.Add(option); 
+
+            if (resolution.width == Screen.currentResolution.width && resolution.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = Array.FindIndex(resolutions, res =>
+    res.width == Screen.currentResolution.width &&
+    res.height == Screen.currentResolution.height);
+            }
+        
+        }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+    
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("MainMenuVolume", volume);
