c96b8e698d871ce6a1bbab11b13b5d350c88c992
Codarcea Alexandru-Christian <alexandru.codarcea@gmail.com>
2025-01-08 01:28:42 +0200

    changed main menu to support 4 maps, added full audio config for every 4 maps

--- Proiect-Unity/Assets/StartMenu/MapSelectionManager.cs
+++ Proiect-Unity/Assets/StartMenu/MapSelectionManager.cs
@@ -6,20 +6,24 @@ public class MapSelectionManager : MonoBehaviour
     public Button mapButton1; 
     public Button mapButton2;
     public Button mapButton3; 
+    public Button mapButton4;
 
     public Image V1; 
     public Image V2;
     public Image V3; 
+    public Image V4;
 
     private void Start()
     {
         V1.gameObject.SetActive(false);
         V2.gameObject.SetActive(false);
         V3.gameObject.SetActive(false);
+        V4.gameObject.SetActive(false);
 
         mapButton1.onClick.AddListener(() => SelectMap(1));
         mapButton2.onClick.AddListener(() => SelectMap(2));
         mapButton3.onClick.AddListener(() => SelectMap(3));
+        mapButton4.onClick.AddListener(() => SelectMap(4));
     }
 
     private void SelectMap(int mapIndex)
@@ -27,6 +31,7 @@ public class MapSelectionManager : MonoBehaviour
         V1.gameObject.SetActive(false);
         V2.gameObject.SetActive(false);
         V3.gameObject.SetActive(false);
+        V4.gameObject.SetActive(false);
 
         switch (mapIndex)
         {
@@ -38,6 +43,9 @@ public class MapSelectionManager : MonoBehaviour
                 break;
             case 3:
                 V3.gameObject.SetActive(true);
+                break;
+            case 4:
+                V4.gameObject.SetActive(true);
                 break;
         }
     }
